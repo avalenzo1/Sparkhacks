4490304393371cdd3fa58ab6f2ffce050704a52e
avalenzo1 <anthonyvalenzo4@gmail.com>
2025-02-08 01:34:16 -0600

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+// import MainContent from './MainContent'
+
 export function App () {
     return (
       <section className='App'>
         <h1>Home</h1>
-        
+        {/* <MainContent /> */}
       </section>
     )
   }
