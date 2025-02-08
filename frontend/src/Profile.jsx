9a1b7059d148eed5c5df408a257f75575198d18c
MatVit2910 <mateoviteri13579@gmail.com>
2025-02-08 02:20:00 -0600

    profile changes

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -12,17 +12,26 @@ function Profile() {
             <Link to="/" className="profile-nav-link">Home</Link>
           </li>
           <li className="profile-nav-item">
-            <Link to="/about" className="profile-nav-link">About</Link>
+            <Link to="/about" className="profile-nav-link">Courses</Link>
           </li>
           <li className="profile-nav-item">
-            <Link to="/services" className="profile-nav-link">Services</Link>
+            <Link to="/services" className="profile-nav-link">Messages</Link>
           </li>
           <li className="profile-nav-item">
-            <Link to="/contact" className="profile-nav-link">Contact</Link>
+            <Link to="/contact" className="profile-nav-link">Log out</Link>
           </li>
         </ul>
       </nav>
     </header>
+    <body className='profileBody'>
+      <div className="profileContent">
+        <img className="profile-picture" src="http://upload.wikimedia.org/wikipedia/commons/9/9b/Photo_of_a_kitten.jpg" alt="cat"/>
+        <div className="profile-info-name">
+          <h1 className="profile-name">John Deere</h1>
+          <h2 className="profile-title">Software Engineer</h2>  
+        </div>  
+      </div>
+    </body>
     </>
     
   )
