01e3cdc1d8e53c3933ab7e0e247e5e2c334dca97
avalenzo1 <anthonyvalenzo4@gmail.com>
2025-02-08 00:43:49 -0600

    added post model

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Task(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
 
     resolved = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["is_deleted", "resolved", "title"]
