72ba8afee825e39fbb38ebe7fa296968126f09d1
MatVit2910 <mateoviteri13579@gmail.com>
2025-02-08 10:05:38 -0600

    profile changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ export function App() {
       <Routes>
         <Route path="/" element={
           <ProtectedRoute>
-            <Home />
+            <Profile />
           </ProtectedRoute>
         }>
 
